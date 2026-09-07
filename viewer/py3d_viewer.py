f763fd7bb76532eefe55cebe54908e9e6be1b1a4
Dopamine-001 <sujoysarkar0300@gmail.com>
2026-09-07 17:57:57 +0530

    Update py3d_viewer.py

--- viewer/py3d_viewer.py
+++ viewer/py3d_viewer.py
@@ -7,30 +7,39 @@ def render_structure(
     pdb_string: str,
     plddt_scores: list = None,
     color_scheme: str = "plddt",
+    representation: str = "Cartoon",
+    color_style: str = "Spectrum",
     width: int = 700,
     height: int = 500,
-) -> None:
-    """Renders a 3D protein structure using py3Dmol within Streamlit."""
+) -> str:
+    """Renders a 3D protein structure using py3Dmol within Streamlit and returns HTML."""
     view = py3Dmol.view(width=width, height=height)
     view.addModel(pdb_string, "pdb")
 
-    if color_scheme == "plddt" and plddt_scores:
-        view.setStyle(
-            {},
-            {
-                "cartoon": {
-                    "colorscheme": {
-                        "prop": "b",
-                        "gradient": "roygb",
-                        "min": 50,
-                        "max": 90,
-                    }
-                }
-            },
-        )
+    # Map representation string to py3Dmol method
+    rep_lower = representation.lower()
+    style_dict = {}
+    if "cartoon" in rep_lower:
+        style_dict["cartoon"] = {}
+    elif "stick" in rep_lower:
+        style_dict["stick"] = {}
     else:
-        view.setStyle({}, {"cartoon": {"color": "spectrum"}})
+        style_dict["cartoon"] = {}
 
+    if color_scheme == "plddt" and plddt_scores:
+        style_dict[list(style_dict.keys())[0]]["colorscheme"] = {
+            "prop": "b",
+            "gradient": "roygb",
+            "min": 50,
+            "max": 90,
+        }
+    else:
+        style_dict[list(style_dict.keys())[0]]["color"] = "spectrum"
+
+    view.setStyle({}, style_dict)
     view.zoomTo()
     html_code = view._make_html()
+    
+    # Check if it's called inside assignment or directly
     components.html(html_code, height=height, width=width)
+    return html_code
